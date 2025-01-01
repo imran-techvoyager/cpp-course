e7e0b7d3982a3fd1f911d0a1fbaf031047550088
imran-techvoyager <imran.techvoyager@gmail.com>
2025-01-01 22:14:24 +0530

    ch2

---
+++ ch2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+int main(){
+
+     int x;
+     //example to write 6 table with for loop
+     for (int i = 1; i <= 10; i++)
+     {
+        cout<<"6 * "<<i<<" = "<<6*i<<endl;
+     }
+     //writing any table user wishes
+
+
+    return 0;
+}
