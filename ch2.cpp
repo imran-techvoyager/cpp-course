ddf0027fda55a4188482ceb409243d67d5f8a6a7
imran-techvoyager <imran.techvoyager@gmail.com>
2025-01-01 22:22:22 +0530

    write any table using for loop

--- ch2.cpp
+++ ch2.cpp
@@ -13,6 +13,14 @@ int main(){
      }
      //writing any table user wishes
 
+    cout<<"enter the number for which you wish to write a table: "<<endl;
+    cin>>x;
+    cout<<"table for "<<x<<" is: "<<endl;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout<<x<<" * "<<i<<" = "<<x*i<<endl;
+    }
+    
 
     return 0;
 }
