f49485bbe7b030c32148211ead39dfd1402d7146
imran-techvoyager <imran.techvoyager@gmail.com>
2025-01-02 21:11:24 +0530

    loops

---
+++ ch4.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+
+
+int main(){
+
+         //loops
+
+         //while loop
+
+         int i = 1;
+
+         while (i<=10)
+         {
+            cout<<"6 * "<<i<<" = "<<i*6<<endl;
+
+             i++;
+         }
+
+         //do while loop
+
+         do
+         {
+            cout<<i<<endl;
+            i++;
+         }
+          while (i<=20);
+
+          //for loop
+
+          for (int j = 0; j <= 10; j++)
+          {
+            cout<<"6 * "<<j<<" = "<<j*6<<endl;
+          }
+          
+         
+         
+
+    return 0;
+}
