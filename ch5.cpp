28088b74b6e9553a8861a355a66da9fc5a83edda
imran-techvoyager <imran.techvoyager@gmail.com>
2025-01-06 11:00:28 +0530

    yess

--- ch5.cpp
+++ ch5.cpp
@@ -13,19 +13,19 @@ int main(){
          cin>>age;
          if ((age<18)&&(age>0))
          {
-            cout<<"you are below 18 not allowed to the party";
+            cout<<"you are below 18 not allowed to the party"<<endl;
          }
          else if (age==18)
          {
-            cout<<"you are 18 and you only get kids pass for the party";
+            cout<<"you are 18 and you only get kids pass for the party"<<endl;
          }
          else if (age<1)
          {
-            cout<<"you are not born yet";
+            cout<<"you are not born yet"<<endl;
          }
          
          else{
-            cout<<"you are an adult and allowed to the party with adults pass";
+            cout<<"you are an adult and allowed to the party with adults pass"<<endl;
          }
 
         //  switch case statment 
@@ -34,13 +34,24 @@ int main(){
         cin>>marks;
         switch (marks)
         {
-        case >90:
+        case 90:
             cout<<"your grade is A";
             break;
-        case >80<90:
+        case 80:
             cout<<"your grade is B";
+            break;
+        case 70:
+            cout<<"your grade is C";
+            break;
+        case 60:
+            cout<<"your grade is D";
+            break;
+        case 40:
+            cout<<"your grade is E";
+            break;
         
         default:
+            cout<<"you are failed";
             break;
         }
          
